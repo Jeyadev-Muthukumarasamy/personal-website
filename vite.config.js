658a751a63c84190e8305d12c7ae41439c025719
Jeyadev <73011515+Jeyadev-Muthukumarasamy@users.noreply.github.com>
2025-01-08 21:21:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://personalwebsite-backend-kwgay7xyj.vercel.app', // Your backend URL
-        changeOrigin: true,  // For virtual hosted sites
-        secure: false,       // For self-signed certificates
-      },
-    },
-    {
-     plugins: [react()],
-  },
-});
+  plugins: [react()],
+})
