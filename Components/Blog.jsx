715e90bb892e9ede035d83f10ecf19b95928b591
Jeyadev <73011515+Jeyadev-Muthukumarasamy@users.noreply.github.com>
2025-01-09 11:08:15 +0530

    Update Blog.jsx

--- Components/Blog.jsx
+++ Components/Blog.jsx
@@ -12,7 +12,7 @@ const Blog = () => {
 
   const fetchBlogs = async () => {
     try {
-      const response = await axios.get ('https://personalwebsite-backend.vercel.app/api/getBlog');
+      const response = await axios.get ('https://personalwebsite-backend-7qicw6h7r.vercel.app/api/getBlog');
       setBlogData(response.data); // Assuming the response is an array of blogs
     } catch (error) {
       console.log(error);
@@ -45,7 +45,7 @@ const Blog = () => {
             </div>
           ))
         ) : (
-          <p className="text-lg font-serif text-black">Will post blogs soon</p>
+          <p className="text-lg font-serif text-black">loading</p>
         )}
       </div>
 
